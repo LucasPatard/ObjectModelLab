e987151974fbec35f6065563bec06268ccd99542
LucasPatard <patard.lucas@gmail.com>
2016-10-19 18:14:43 +0200

    ...

--- src/model/index.js
+++ src/model/index.js
@@ -1,2 +1,46 @@
 
-/* TODO : Créer le modèle objet ici */ 
+class Data{
+    constructor(){
+    }
+
+}
+class Datum extends Data{
+    constructor(data){
+        super();
+        this.value = data.value
+    }
+    get value(){
+        return this.value;
+    }
+    set value(val){
+        this.value = val;
+    }
+}
+class TimeSeries extends Data{
+    constructor(data){
+        super();
+        this.values= data.values;
+        this.label = data.label;
+    }
+    get values(){
+        return this.values;
+    }
+    set values(val){
+        this.values = val;
+    }
+    get label(){
+        return this.label;
+    }
+    set label(val){
+        this.label = val;
+    }
+}
+class Sensor{
+    constructor(data){
+        this.id = data .id
+        this.name = data.name
+        this.data = new Data(data)
+
+    }
+
+}
--- test/model/index.js
+++ test/model/index.js
@@ -5,6 +5,8 @@ import expect from 'expect';
 import  { data }  from './sensors_data'
 console.log(data.length);
 
+Sensor(data[0]);
+
 describe('Sensor model tests', () => {
   /* TODO: Ecrire ici la suite de tests pourle modèle objet.*/
 });
