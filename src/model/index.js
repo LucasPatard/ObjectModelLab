77b7496a7dd22a2e8c1f9e098a9e7465a69ebfb6
LucasPatard <patard.lucas@gmail.com>
2016-10-19 22:00:58 +0200

    done

--- src/model/index.js
+++ src/model/index.js
@@ -1,46 +1,74 @@
-
-class Data{
+export const enumeration = ["TEMPERATURE","HUMIDITY","LIGHT","DOOR","FAN_SPEED","NO_TYPE"];
+export class Data{
     constructor(){
     }
 
 }
-class Datum extends Data{
+ export class Datum extends Data{
     constructor(data){
         super();
         this.value = data.value
     }
     get value(){
-        return this.value;
+        return this._value;
     }
     set value(val){
-        this.value = val;
+        this._value = val;
     }
 }
-class TimeSeries extends Data{
+export class TimeSeries extends Data{
     constructor(data){
         super();
         this.values= data.values;
-        this.label = data.label;
+        this.labels = data.labels;
     }
     get values(){
-        return this.values;
+        return this._values;
     }
     set values(val){
-        this.values = val;
+        this._values = val;
     }
-    get label(){
-        return this.label;
+    get labels(){
+        return this._labels;
     }
-    set label(val){
-        this.label = val;
+    set labels(val){
+        this._labels = val;
     }
 }
-class Sensor{
+export class Sensor{
     constructor(data){
-        this.id = data .id
+        this.id = data.id
         this.name = data.name
-        this.data = new Data(data)
+        if(enumeration.includes(data.type)) this.type = data.type;
+        else this.type='NO_TYPE';
 
+        if(isNaN(data.data.value)) this.data = new TimeSeries(data.data);
+        else if(isNaN(data.data.values)) this.data = new Datum(data.data);
+
+    }
+    get id(){
+        return this._id;
+    }
+    set id(val){
+        this._id = val;
+    }
+    get name(){
+        return this._name;
+    }
+    set name(val){
+        this._name = val;
+    }
+    get data(){
+        return this._data;
+    }
+    set data(val){
+        this._data = val;
+    }
+    get type(){
+        return this._type;
+    }
+    set type(val){
+        this._type = val;
     }
 
 }
--- test/model/index.js
+++ test/model/index.js
@@ -1,12 +1,63 @@
 import expect from 'expect';
 
-// import { ??? } from '../../src/model';
+import {Sensor,
+        Data,
+        Datum,
+        TimeSeries,
+        enumeration
+}from '../../src/model';
 
 import  { data }  from './sensors_data'
 console.log(data.length);
 
-Sensor(data[0]);
-
-describe('Sensor model tests', () => {
-  /* TODO: Ecrire ici la suite de tests pourle modèle objet.*/
+console.log(typeof (enumeration))
+describe('Test class Sensor ', () => {
+     let s = new Sensor(data[0])
+    it('Sensor doit posseder un id qui est un nombre', () => {
+        expect(s.id).toBeA('number');
+    });
+    it('Sensor doit posseder un nom qui est une chaine', () => {
+        expect(s.name).toBeA('string');
+    });
+    it('Sensor doit pointer sur un object de type Data', () => {
+        expect(s.data).toBeA(Data);
+    });
+    it('Sensor doit avoir un type contenus dans enumeration', () => {
+        expect(enumeration).toInclude(s.type);
+    });
 });
+
+describe('Test heritage Data ', () => {
+    let s = new Sensor(data[0])
+    it('TimeSeries doit heriter de Data', () => {
+        expect(s.data).toBeA(Data);
+    });
+    s = new Sensor(data[1])
+    it('Datum doit heriter de Data', () => {
+        expect(s.data).toBeA(Data) ;
+    });
+
+});
+
+describe('Test class Datum ', () => {
+    let s = new Sensor(data[1])
+    it('Datum doit posseder une value qui est un nombre', () => {
+        expect(s.data.value).toBeA('number');
+    });
+});
+
+describe('Test class TimeSeries ', () => {
+    let s = new Sensor(data[0])
+    it('TimeSeries doit posseder values qui est un tableau d entiers', () => {
+        expect(s.data.values).toBeA(Array) && s.data.values.every(value => expect(value).toBeA('number'));
+    });
+
+    it('TimeSeries doit posseder labels qui est un tableau de chaine', () => {
+        expect(s.data.labels).toBeA(Array) && s.data.labels.every(value => expect(value).toBeA('string'));
+    });
+
+});
+
+
+
+
